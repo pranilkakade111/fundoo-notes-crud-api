324215b34bd23ca13877866b853201719a6c3782
Pranil Kakade <pranilkakade111@gmail.com>
2021-05-11 09:01:31 +0530

    [Added]: Added Business Logic for CRUD And Trashed API

--- app/services/note.js
+++ app/services/note.js
@@ -1,11 +1,27 @@
-const noteModel = require('../models/note');
-const helper = require('../../utility/helper');
+const note = require('../models/note');
+const notemodel = require('../models/note');
 
-class NoteServices {
-    createNote = (noteInfo, callback) => {
-      console.log(noteInfo);
-      noteModel.createNote(noteInfo, callback);
-    };
+class NoteService {
+  createNote = (noteInfo, callback) => {
+      notemodel.createNote(noteInfo, callback);
+  };
+   
+  updateNote = (noteData, callback) => {
+      notemodel.updateNote(noteData, callback);
+  };
+
+  getNote = (callback) => {
+      notemodel.getNote(callback);
+  };
+
+  deleteNote = (noteIds, callback) => {
+      notemodel.deleteNote(noteIds, callback);
+  };
+
+  trashNote = (NoteIDs, callback) => {
+      notemodel.trashNote(NoteIDs, callback);
+  };
 }
 
-module.exports = new NoteServices();
+
+module.exports = new NoteService();
