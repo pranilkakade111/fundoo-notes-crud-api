9f68420a8fdc62f4984cf3346a1408c8245f71d2
Pranil Kakade <pranilkakade111@gmail.com>
2021-05-11 09:03:15 +0530

    [Added]: Added models for CRUD And Trashed API

--- app/models/note.js
+++ app/models/note.js
@@ -1,36 +1,71 @@
-const { string } = require('@hapi/joi');
 const mongoose = require('mongoose');
-const User = require('./user');
+const note = require('../services/note');
 
-const NoteSchema = mongoose.Schema({
+const noteSchema = mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
   isPinned: { type: Boolean, default: false },
   isArchived: { type: Boolean, default: false },
+  isReminder: { type: Boolean, default: false },
   isTrashed: { type: Boolean, default: false },
-  isreminder: { type: Boolean, default: false },
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: false,
-    ref: 'User',
-    unique: true,
-  },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
 }, {
   timestamps: true,
   versionKey: false,
 });
 
-const noteModel = mongoose.model('Note', NoteSchema);
+const noteModel = mongoose.model('Note', noteSchema);
 
 class NoteModel {
-    createNote = (noteInfo, callback) => {
-        const note = new noteModel({
-          title: noteInfo.title,
-          description: noteInfo.description,
-        });
-        console.log(note);
-        note.save(callback);
-    };
+  createNote = (noteInfo, callback) => {
+    const note = new noteModel({
+      title: noteInfo.title,
+      description: noteInfo.description,
+      userId: noteInfo.userId
+    });
+    note.save(callback);
+  };
+
+  updateNote = (notesID, callback) => {
+    noteModel.findByIdAndUpdate(notesID.noteId, {
+      title: notesID.title,
+      description: notesID.description,
+    }, {new: true}).then((noteone) => {
+        callback(null, noteone);
+    }).catch((err) => {
+        callback(err, null);
+    });   
+  };
+
+  getNote = (callback) => {
+      noteModel.find((err, notedata) => {
+        if(err){
+            callback(err ,null);
+          }else {
+            callback(null ,notedata);
+          }
+      });     
+  };
+
+  deleteNote = (noteIds, callback) => {
+      noteModel.findByIdAndRemove(noteIds, (err, noteresult) => {
+          if(err){
+              callback(err, null);
+         }else {
+             callback(null, noteresult);
+         }
+      });
+  };
+
+  trashNote = (data, callback) => {
+      noteModel.findByIdAndUpdate(data, {isTrashed: true}, {new: true})
+      .then((note) => {
+          callback(null, note);
+      }).catch((err) => {
+          callback(err, null);
+      });
+  };
+
 }
 
 module.exports = new NoteModel();
