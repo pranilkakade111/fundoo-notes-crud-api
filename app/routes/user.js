3541b24eeb74c596733df683d3891b8434b18bc2
Pranil Kakade <pranilkakade111@gmail.com>
2021-05-11 08:56:29 +0530

    [Feat]: Added Routes For CRUD And Trashed API

--- app/routes/user.js
+++ app/routes/user.js
@@ -1,4 +1,17 @@
+/** ***********************************************************************
+ * Execution        : 1. default node       cmd> nodemon server.js
+ *
+ * Purpose          : to hit the perticular API
+
+ * @file            : user.js
+ * @author          : Pranil Kakadea
+ * @version         : 1.0
+ * @since           : 02-05-2021
+ *
+ ************************************************************************* */
 const user = require('../controllers/user');
+const note = require('../controllers/note');
+const { verifyToken } = require('../../utility/helper');
 
 module.exports = (app) => {
   app.post('/user', user.createUser);
@@ -7,5 +20,15 @@ module.exports = (app) => {
 
   app.post('/forgotPassword', user.forgotPassword);
 
-  app.post('/resetPassword', user.resetPassword);
+  app.post('/resetPassword', verifyToken, user.resetPassword);
+
+  app.post('/notes', verifyToken, note.createNote);
+
+  app.put('/notes/:noteId', verifyToken, note.updateNote);
+
+  app.get('/notes', verifyToken, note.getNote);
+
+  app.delete('/notes/:noteId', verifyToken, note.deleteNote);
+
+  app.put('/notes/trash/:noteId', verifyToken, note.trashNote);
 };
