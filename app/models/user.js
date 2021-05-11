1c8621775f0640cb5438e115b5b769fda7dda947
Pranil Kakade <pranilkakade111@gmail.com>
2021-05-11 09:02:30 +0530

    [Refactor]: Refactor models For Login,Forgot Password And Reset Password API

--- app/models/user.js
+++ app/models/user.js
@@ -1,3 +1,12 @@
+/** ***********************************************************************
+ * Execution        : 1. default node       cmd> nodemon server.js
+ *
+ * Purpose          : To Create Database Schema For API
+ * @file            : user.js
+ * @author          : Pranil Kakade
+ * @version         : 1.0
+ * @since           : 02-05-2021
+ ************************************************************************* */
 const { required } = require('@hapi/joi');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
@@ -6,9 +15,11 @@ const UserSchema = mongoose.Schema({
     firstName :{type: String ,required: true}, 
     lastName :{type: String ,required: true},
     email : {type: String , unique: true ,required: true},
-    password : {type: String ,required: true}
+    password : {type: String ,required: true},
+    
 }, {
-    timestamps: true
+    timestamps: true,
+    versionKey: false
 });
 
 UserSchema.pre('save' ,async function (next) {
@@ -29,25 +40,33 @@ const userModel = mongoose.model('User' ,UserSchema);
 class UserModel {
     createUser = (userData ,callback) => {
         const userr = new userModel(userData);
-        userr.save((err, userResult)=> {
+        userr.save((err, userResult) => {
             (err) ? callback(err ,null) : callback(null ,userResult);            
         });
     };
 
     loginUser = (userLogin, callback) => {
-        userModel.findOne({ email: userLogin.email }, callback)
+        userModel.findOne({ email: userLogin.email })
+        .then((user)=>{
+            callback(null, user);
+        });
     };
 
     forgotPassword = (data ,callback) => {
-        userModel.findOne({email: data.email } ,callback)
+        userModel.findOne({email: data.email })
+        .then((userOne)=>{
+            callback(null, userOne);
+        });
     }
 
     resetPassword = async (data, callback) => {
-        const salt = await bcrypt.genSalt(10);
-        const encrypt = await bcrypt.hash(data.password, salt);
-        userModel.findOneAndUpdate({ email: data.email }, { password: encrypt } ,{new: true} ,callback)
-          
-      }
+        const salt = await bcrypt.genSalt(10)
+        const encrypt = await bcrypt.hash(data.password , salt) 
+        userModel.findOneAndUpdate({ email: data.email }, { password: encrypt } ,{new: true})
+        .then((cred)=>{
+            callback(null, cred);
+        }); 
+      };
 
 }
-module.exports = new UserModel;
+module.exports = new UserModel();
