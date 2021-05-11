b788d6672ae28715e28f96107029c57257e40a96
Pranil Kakade <pranilkakade111@gmail.com>
2021-05-11 08:59:46 +0530

    [Added]: Added CRUD And Trashed API

--- app/controllers/note.js
+++ app/controllers/note.js
@@ -1,48 +1,119 @@
 const noteServices = require('../services/note');
-const jwt = require('jsonwebtoken');
 
-class NoteController {
+class NoteController 
+{
   createNote = (req, res) => {
-    
-    
-    try {
-        if (!req.body.title || !req.body.description) {
-            return res.status(400).send({
-            success: false,
-            message: "Fileds Cannot Be Empty...!!!"
-            });
-        }
-        const noteInfo = {
-            title: req.body.title,
-            description: req.body.description,
-            
-        };
+     if(!req.body.title||!req.body.description){
+       return res.status(400).send({
+           success: false,
+           message: 'Fields Can not Be Empty....!!!'
+       });
+     };
+       const noteInfo = {
+           title: req.body.title,
+           description: req.body.description,
+           userId : req.userId    
+       }
        
-        noteServices.createNote(noteInfo, (err, data) => {
-            if(err) {
-                return res.status(500).send({
-                    success: false,
-                    message: 'Some Error Occured While Creating Notes...!!! ',
-                    err,  
-                });
-            } else {
-                return res.status(200).send({
-                    success: true,
-                    message: 'Note Added Successfully...!!!',
-                    data: data,
-                
-                }); 
-            }
-        });
+       noteServices.createNote(noteInfo, (err, data) => {
+         if(err){
+             return res.status(401).send({
+                 success: false,
+                 message: 'Failed To Create Note...!!!',
+                 err,
+             });
+         } else {
+             return res.status(200).send({
+                 success: true,
+                 message: 'Note Created Successfully....!!!',
+                 data: data,
+             });
+         }
+       });
+  };
 
-        } catch (error) {
-            res.status(400).send({
+  updateNote = (req, res) => {
+    if(!req.body.title||!req.body.description){
+        return res.status(400).send({
+            success: false,
+            message: 'Fields Can not Be Empty....!!!'
+        });
+      };
+
+      const noteData = {
+          title: req.body.title,
+          description: req.body.description,
+          noteId: req.params.noteId
+      }
+
+      noteServices.updateNote(noteData, (err, noteResult) => {
+        if(noteResult === null) {
+            return res.status(404).send({
                 success: false,
-                message: "Some error occurred while creating note!!!!",
+                message: 'Note Not Found With An Id' + res.params.noteId,
+                err,
+            });
+        } else {
+            return res.status(200).send({
+                success: true,
+                message: 'Note Found And Updated Successfully....!!!',
+                data: noteResult
             });
         }
-    };
+      });
+  };
 
+  getNote = (req, res) => {
+    noteServices.getNote((err, noteResult) => {
+      if(err) {
+          return res.status(400).send({
+              success: false,
+              message: 'Failed To Retriving All Notes....!!!!'
+          });
+      } else {
+          return res.status(200).send({
+              success: true,
+              message: 'Retrived All note Successfully.....!!!!',
+              data: noteResult
+          });
+      }
+    }); 
+  };
+
+  deleteNote = (req, res) => {
+      const nId = req.params.noteId;
+    noteServices.deleteNote(nId, (err, noteResult) => {
+        if(noteResult === null){
+            return res.status(404).send({
+                success: false,
+                message: 'Note not Found With An Id..!!' + nId,
+                
+            });
+        }else {
+            return res.status(200).send({
+              success: true,
+              message: 'Note Deleted Successfully....!!!!'
+            });
+        }
+    });  
+  };
+
+  trashNote = (req, res) => {
+    const NoteID = req.params.noteId;
+    noteServices.trashNote(NoteID, (err, noteResult) => {
+        if(noteResult === null){
+            return res.status(404).send({
+                success: false,
+                message: 'Note not Found With An Id..!!' + NoteID,
+            });
+        }else {
+            return res.status(200).send({
+                success: true,
+                message: 'Note Is Move To Trash Successfully....!!!!'
+              });
+        }
+    });
+  };
 }
 
 module.exports = new NoteController();
